076f77391a61eb87ea01efaa34a166acf126b20a
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-04-21 18:51:35 +0000

    Modify homepage and it's styling

--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -5,24 +5,30 @@ export function loadHomePageContent() {
 
   content.innerHTML = "";
 
+  const homeDiv = document.createElement("div");
+  homeDiv.classList.add("home");
+
+  const headlineDiv = document.createElement("div");
   const headline = document.createElement("h2");
   headline.textContent = "Welcome to Strange World Restaurant";
+  headlineDiv.appendChild(headline);
 
+  const restaurantImageDiv = document.createElement("div");
   const restaurantImage = document.createElement("img");
   restaurantImage.id = "restaurant-image";
   restaurantImage.src = ImageSrc;
   restaurantImage.alt = "Restaurant Image";
+  restaurantImageDiv.appendChild(restaurantImage);
 
-  const restaurantInfo = document.createElement("div");
-  restaurantInfo.id = "restaurant-info";
-
-  const paragraph = document.createElement("p");
-  paragraph.textContent =
+  const restaurantInfoDiv = document.createElement("div");
+  const restaurantInfo = document.createElement("p");
+  restaurantInfo.textContent =
     "Experience the finest dining experience at our restaurant. We take pride in offering a diverse menu of high-quality dishes prepared with fresh, locally-sourced ingredients. Whether you're looking for a romantic dinner, a family gathering, or a casual meal with friends, we have something for everyone.";
+  restaurantInfoDiv.appendChild(restaurantInfo);
 
-  restaurantInfo.appendChild(paragraph);
+  homeDiv.appendChild(headlineDiv);
+  homeDiv.appendChild(restaurantImageDiv);
+  homeDiv.appendChild(restaurantInfoDiv);
 
-  content.appendChild(headline);
-  content.appendChild(restaurantImage);
-  content.appendChild(restaurantInfo);
+  content.append(homeDiv);
 }
